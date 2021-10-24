e8d3486b5f133fabca3563bffd1543693b72c5a2
Bokov <bokoff.il@gmail.com>
2021-10-24 15:03:00 +0300

    Добавлен фрейм для добавления юзера, настроены все компоненты фрейма

--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
 NAME='Хуальфа банк'
 GEOMETRY='800x600'
+HEAD_FONT=("Courier", 32, "bold")
+TEXT_FONT=("Courier", 18, "bold")
+BACKGROUND_COLOR=None
--- widgets.py
+++ widgets.py
@@ -1,4 +1,8 @@
-from tkinter import Menu
+import tkinter
+from tkinter import Menu,Frame, Label, Entry, LabelFrame, Button
+from tkinter import messagebox
+from tkinter import StringVar
+import settings
 
 
 class MainMenu(Menu):
@@ -31,4 +35,102 @@ class MainMenu(Menu):
         self.add_cascade(label='Операции', menu=transaction_menu)
 
         root.config(menu=self)
+
+
+class BaseFrame(Frame):
+    def __init__(self, root):
+        Frame.__init__(self, root)
+        self.background_color=settings.BACKGROUND_COLOR
+        self.configure(bg=self.background_color)
+    def pack_frame(self):
+        self.pack(padx=20, pady=20, fill='both',expand="YES")
+
+
+class HeadLabel(Label):
+    def __init__(self, root, text):
+        Label.__init__(self,root)
+        self.configure(font=settings.HEAD_FONT, text=text, bg=root.background_color)
+
+
+class InfoLabel(Label):
+    def __init__(self, root, text):
+        Label.__init__(self,root)
+        self.configure(font=settings.TEXT_FONT, text=text, bg=root.background_color)
+
+
+class InputButton(Button):
+   def __init__(self,root, text, command):
+       Button.__init__(self,root)
+       self['text']=text
+       self['font']=settings.TEXT_FONT
+       self['command'] = command
+       self.pack(pady=5)
+
+
+class InputFrame(BaseFrame):
+    def __init__(self, root, text, var):
+        BaseFrame.__init__(self, root)
+        self.label=InfoLabel(self, text)
+        self.input=Entry(self, font=settings.TEXT_FONT, textvariable=var)
+
+    def pack_frame(self):
+        self.label.pack(side='left')
+        self.input.pack(side='left',expand='yes',fill='x')
+        self.pack(anchor='w', fill='x')
+        return self
+
+    def clean(self):
+        self.input.delete(0,tkinter.END)
+
+
+class AboutBankFrame(BaseFrame):
+    def __init__(self, root):
+        BaseFrame.__init__(self,root)
+        HeadLabel(self, 'Банк').pack()
+
+
+class AddPersonFrame(BaseFrame):
+    def __init__(self, root):
+        BaseFrame.__init__(self, root)
+        HeadLabel(self, 'Добавить пользователя').pack()
+
+        self.name=StringVar()
+        self.surname = StringVar()
+        self.date_birthday = StringVar()
+        self.email = StringVar()
+        self.phone = StringVar()
+
+        self.fr=LabelFrame(self)
+
+        self.list_frame=[
+            InputFrame(self.fr,'Имя: ',self.name),
+            InputFrame(self.fr, 'Фамилия: ', self.surname),
+            InputFrame(self.fr, 'Дата рождения: ', self.date_birthday),
+            InputFrame(self.fr, 'Email: ', self.email),
+            InputFrame(self.fr, 'Телефон: ', self.phone)
+        ]
+
+        for frame in self.list_frame:
+            frame.pack_frame()
+
+        self.fr.pack(anchor='w',fill='x')
+        InputButton(self, 'Добавить', command=self.get_input_data)
+
+    def get_input_data(self):
+        name=self.name.get()
+        surname=self.surname.get()
+        date_birthday=self.date_birthday.get()
+        email=self.email.get()
+        phone=self.phone.get()
+
+        for frame in self.list_frame:
+            frame.clean()
+        messagebox.showinfo('Успех', 'Пользователь успешно добавлен!')
+        print(name,surname,date_birthday,email,phone)
+
+
+
+
+
+
 
